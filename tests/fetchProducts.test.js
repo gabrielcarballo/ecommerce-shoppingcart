4963e4c7aef859a3d8a1c1daeda138313b931d65
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-11 22:47:34 -0300

    Requisite 1 done. Made a mistake calling fetch func directly on script.js. Fixing on next commits

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,30 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Testa se fetch foi chamada quando fetchProducts for invocada com computador como parâmetro', async () => {
+    const search = 'computador'
+    const searchEndpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${search}`
+    await fetch(searchEndpoint)
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('Testa se o fetch está chamando a URL correta quando passado computador como parâmetro', async () => {
+    const search = 'computador'
+    const searchEndpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${search}`
+    await fetchProducts('computador')
+    expect(fetch).toBeCalledWith(searchEndpoint);
+  });
+  it('Teste se o dado retornado quando chamada a função fetchProducts passando computador está correta', async () => {
+    const test = await fetchProducts('computador')
+    expect(test).toEqual(computadorSearch)
+  });
+  it('Testa se retorna o erro: You must provide an url se chamada a função sem parâmetro', async () => {
+    try{
+      await fetchProducts()
+    } catch (error){
+      expect(error).toEqual(new Error('You must provide an url'));
+    }
+  });
 });
