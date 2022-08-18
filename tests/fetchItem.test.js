fd4bd879120e6dce4bef52e1c90fd706bc05a76e
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-18 17:00:58 -0300

    Req 3 n 4 done

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -8,11 +8,26 @@ describe('2 - Teste a função fetchItem', () => {
   });
 
   it('Teste se a função fetchItem executa a função fetch quando invocada', async () => {
-   await fetchItem('MLB1615760527');
-   expect(fetch).toHaveBeenCalled();
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
   });
 
   it('Testa se o retorno de fetchItem é a estrutura do objeto esperado', async () => {
     expect(await fetchItem('MLB1615760527')).toEqual(item);
-  })
+  });
+
+  it('Testa se a função fetchItem executa a função fetch com o endpoint correto', async () => {
+    const expectedEndpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem('MLB1615760527')
+    expect(fetch).toBeCalledWith(expectedEndpoint);
+  });
+
+  it('Testa se o erro retorna corretamente quando fetchItem é chamada sem parâmetro', async () => {
+    try {
+      await fetchItem();
+    } catch (error) {
+      expect(error).toEqual(new Error('You must provide an url'));
+    }
+  });
+
 });
